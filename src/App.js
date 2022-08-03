7b721fc443a835efcfccc6cdae362f2d857ec5d1
NicolasBorlet <75842491+NicolasBorlet@users.noreply.github.com>
2022-08-03 17:13:16 +0200

    Yop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const apidata = "https://jsonplaceholder.typicode.com/posts";
+
+const App = () => {
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(apidata); //Je récupère les données de l'api
+      const data = response.data; //J'isole les datas
+
+      data.map((user) => {
+        //Création de la boucle affichant les posts de tous les users
+        const user1 = user; // Création variable de l'user 1
+
+        // Condition qui me permet d'afficher uniquement les posts de l'user 1
+        if (user1.userId === 1) {
+          // Supression du premier et dernier post
+          if (user1.id > 1 && user1.id < 10) {
+            console.log(user1);
+          }
+        }
+      });
+    }
+    getData();
+  });
+  return;
+};
 
 export default App;
